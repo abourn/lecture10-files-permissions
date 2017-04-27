cf04ca685ba46516f723e3991548f98b29fe750a
Adam Bourn <abourn@uw.edu>
2017-04-27 15:20:57 -0700

    finish lecture

--- app/src/main/java/edu/uw/filedemo/MainActivity.java
+++ app/src/main/java/edu/uw/filedemo/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.uw.filedemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -13,11 +19,22 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main";
 
     private static final String FILE_NAME = "myFile.txt";
+    private static final int REQUEST_WRITE_CODE = 1;
     
     private EditText textEntry; //save reference for quick access
     private RadioButton externalButton; //save reference for quick access
@@ -36,7 +53,17 @@ public class MainActivity extends AppCompatActivity {
 
         if(externalButton.isChecked()){ //external storage
 
-            saveToExternalFile(); //helper method
+            if (isExternalStorageWritable()) { // check to make sure we can actually access the storage
+                // need permission in both the manifest, and in the code (at runtime)
+                int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED) { // if the user has given us permission
+                    // we have permission to do stuff
+                    saveToExternalFile();
+                } else { // we don't have permission yet
+                    // we need to ask for permission
+                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_CODE);
+                }
+            }
 
         }
         else { //internal storage
@@ -46,11 +73,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //actually write to the file
-    private void saveToExternalFile(){
-
+    private void saveToExternalFile() {
+        try {
+            File dir = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            } // create folder if doesn't exist
+            File file = new File(dir, FILE_NAME);
+            Log.v(TAG, "Saving to " + file.getAbsolutePath());
+            PrintWriter out = new PrintWriter(new FileWriter(file, true));
+            out.println(textEntry.getText().toString());
+            out.close(); // remember to always close to file to avoid memory/resource leaks
+        } catch (IOException e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
     }
 
 
+    // listen for when the user grants permission to write to file system
+    @Override                                               // two parallel arrays--the permission we requested and whether they were granted or not
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case REQUEST_WRITE_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // now you have permission to do your thing
+                    saveToExternalFile();
+                }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
     public void loadFile(View v){
         Log.v(TAG, "Loading file...");
@@ -58,13 +110,44 @@ public class MainActivity extends AppCompatActivity {
         textDisplay.setText(""); //clear initially
 
         if(externalButton.isChecked()){ //external storage
+            try {
+                File dir = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+                File file = new File(dir, FILE_NAME);
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                StringBuilder text = new StringBuilder();
 
+                //read the file
+                String line = reader.readLine();
+                while (line != null) {
+                    text.append(line + "\n");
+                    line = reader.readLine();
+                }
+
+                textDisplay.setText(text.toString());
+                reader.close();
+            } catch (IOException ioe) {
+                Log.d(TAG, Log.getStackTraceString(ioe));
+            }
         }
         else { //internal storage
+            // we don't need permission to access our Application's own internal storage
 
+            File dir = getFilesDir();
+            //FileOutputStream fos = openFileInput(FILE_NAME, MODE_PRIVATE); // skipped for time, see completed branch
+//            getCacheDir(); // internal cache
+//            getExternalCacheDir();
         }
     }
 
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+
+        }
+        return false;
+        // return Environment.MEDIA_MOUNTED.equals(state); // You can probably do this, he just didn't.
+    }
 
     public void shareFile(View v) {
         Log.v(TAG, "Sharing file...");
--- app/src/main/java/edu/uw/filedemo/PhotoActivity.java
+++ app/src/main/java/edu/uw/filedemo/PhotoActivity.java
@@ -2,18 +2,29 @@ package edu.uw.filedemo;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PhotoActivity extends AppCompatActivity {
 
     private static final String TAG = "Photo";
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Uri pictureFileUri;
+    private Uri mediaStoreUri;
 
 
     @Override
@@ -28,19 +39,57 @@ public class PhotoActivity extends AppCompatActivity {
     public void takePicture(View v){
         Log.v(TAG, "Taking picture...");
 
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null)
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        // making a file for the camera to save the picture it takes inside
+        File file = null;
+        try {
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); //include timestamp
+
+            //ideally should check for permission here, skipping for time
+            File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            file = new File(dir, "PIC_"+timestamp+".jpg");
+            boolean created = file.createNewFile(); //actually make the file!
+            Log.v(TAG, "File created: "+created);
+
+        } catch (IOException ioe) {
+            Log.d(TAG, Log.getStackTraceString(ioe));
+        }
+
+        if (file != null) {
+            pictureFileUri = Uri.fromFile(file);
+
+            // initially, our file path looks kinda like file://storage/emulator/0/Picture/mypic.jpg
+            // that's why the emulator would crash on trying to share pictures, so the solution ends up
+            // being that we make the picture have a content:// thing.  So we use a contentProvider:
+            // and then in sharePicture, make sure that you are putExtra on shareIntent the mediaStoreUri and not the pictureFileUri
+
+            MediaScannerConnection.scanFile(this, new String[] {file.toString()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            mediaStoreUri = uri;  //save the content:// Uri for later
+                            Log.v(TAG, "MediaStore Uri: "+uri);
+                        }
+                    });
+
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            // the camera says: If you give me this particular extra, I'll know where to save the image to.
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureFileUri);
+
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap)extras.get("data");
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap)extras.get("data");
             ImageView imageView = (ImageView)findViewById(R.id.imgThumbnail);
-            imageView.setImageBitmap(imageBitmap);
+            imageView.setImageURI(pictureFileUri); // put in full resolution picture
+//            imageView.setImageBitmap(imageBitmap);
 
         }
         super.onActivityResult(requestCode, resultCode, data);
@@ -48,6 +97,14 @@ public class PhotoActivity extends AppCompatActivity {
 
     public void sharePicture(View v){
         Log.v(TAG, "Sharing picture...");
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("image/*"); // put on the envelope "only handle this intent if you can handle pictures, if not, don't even bother
+        shareIntent.putExtra(Intent.EXTRA_STREAM, mediaStoreUri);
+
+        Intent chooser = Intent.createChooser(shareIntent, "Share My Picture");
+        startActivity(chooser);
+
 
     }
 }
